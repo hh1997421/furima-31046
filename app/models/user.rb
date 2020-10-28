b5d4d3548133e4858dccdcb1f23b19f982627476
hh1997421 <haruto.10120815@gmail.com>
2020-10-28 17:14:37 +0900

    正規表現に変数　修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :nickname,           presence: true
-  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])\w{6}\z/
+
+  validates :nickname, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)\w{6}\z/.freeze
   validates :password, presence: true,
-            format: { with: VALID_PASSWORD_REGEX}
-  validates :last_name,          presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' } # ここがユーザー本名全角の正規表現
-  validates :first_name,         presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' } # ここがユーザー本名全角の正規表現
-  validates :last_name_kana,     presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.'} # ここがフリガナ全角の正規表現
-  validates :first_name_kana,    presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' } # ここがフリガナ全角の正規表現
+                       format: { with: VALID_PASSWORD_REGEX }
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  validates :last_name,          presence: true, format: { with: VALID_NAME_REGEX, message: 'is invalid. Input full-width characters.' } # ここがユーザー本名全角の正規表現
+  validates :first_name,         presence: true, format: { with: VALID_NAME_REGEX, message: 'is invalid. Input full-width characters.' } # ここがユーザー本名全角の正規表現
+  VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :last_name_kana,     presence: true, format: { with: VALID_KANA_REGEX , message: 'is invalid. Input full-width katakana characters.' } # ここがフリガナ全角の正規表現
+  validates :first_name_kana,    presence: true, format: { with: VALID_KANA_REGEX , message: 'is invalid. Input full-width katakana characters.' } # ここがフリガナ全角の正規表現
   validates :birth_date,         presence: true
 end
